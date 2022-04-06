c0f0b2daa0c4148a3a69c7a34a1ad4aa227ff440
Precious-B <solomonprecious04@gmail.com>
2022-04-06 20:49:28 +0000

    Corrections

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,11 @@
-export default function getFullResponseFromAPI(success){
-  return new Promise((resolve, reject) => {
-    if (success){
-     resolve({
-       status: 200,
-       body: 'Success',
-     });
-    } else{
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, fail) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
       fail(new Error('The fake API is not working currently'));
     }
   });
